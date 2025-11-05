454b19f585ad49bb9b9a75137f19c02d66f658e9
Jan Friedrich <jan.friedrich@komsa.de>
2025-11-05 11:17:16 +0100

    #551059 Relinq nach Git migrieren

--- AssemblyInfoShared.cs
+++ AssemblyInfoShared.cs
@@ -23,5 +23,5 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyConfiguration("Build type: Debug")]
 [assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.9")]
-[assembly: AssemblyInformationalVersion("2.1.0-x.9 Debug; ")]
+[assembly: AssemblyFileVersion("2.1.0.11")]
+[assembly: AssemblyInformationalVersion("2.1.0-x.11 Debug; ")]
